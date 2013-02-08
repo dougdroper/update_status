da3f33c3d976a57825cd7f143bebbd3d7503ed2c
Douglas Roper <douglasroper@notonthehighstreet.com>
2013-02-08 17:06:36 +0000

    Refactor Jenkins a bit

--- spec/status/jenkins_spec.rb
+++ spec/status/jenkins_spec.rb
@@ -5,31 +5,28 @@ describe Status::Jenkins do
   subject {Status::Jenkins.new("")}
 
   before do
-    Status.stub(:config => stub(:attrs => {}))
-
+    Status.stub(:config => stub(:attrs => {}, :parsed => {}))
     stub(Status::Request)
+    Status.stub(:system_warn)
   end
 
   context "#state" do
     it "changes slashes to underscores" do
-      Status::Jenkins.new("dr/feature").path.should == "/job/dr_feature/lastBuild/api/json"
+      Status::Jenkins.new("dr/feature").send(:path).should == "/job/dr_feature/lastBuild/api/json"
     end
 
     it "is success when ci result is success" do
       Status::Request.stub(:new => stub(:get => {"building" => false, "result" => "success"}))
-      subject.instance_variable_set("@build_url", true)
       subject.state.should == "success"
     end
 
     it "is pending when ci result is building" do
       Status::Request.stub(:new => stub(:get => {"building" => true, "result" => "success"}))
-      subject.instance_variable_set("@build_url", true)
       subject.state.should == "pending"
     end
 
     it "is pending when ci result is in any other state" do
       Status::Request.stub(:new => stub(:get => {"building" => true, "result" => "failed"}))
-      subject.instance_variable_set("@build_url", true)
       subject.state.should == "pending"
     end
 
@@ -40,8 +37,26 @@ describe Status::Jenkins do
 
     it "is failure when ci result is failure" do
       Status::Request.stub(:new => stub(:get => {"building" => false, "result" => "failure"}))
-      subject.instance_variable_set("@build_url", true)
       subject.state.should == "failure"
+    end
+  end
+
+  context "#target_url" do
+    it "has the last build in the path as default" do
+      Status::Request.stub(:new => stub(:get => {"builds" => []}))
+      Status::Jenkins.new("dr/feature", '481002a').target_url.should == "/job/dr_feature"
+    end
+
+    it "has the last build number in the path as when last build contains sha" do
+      build = {"number" => 12, "actions" => [{},{"lastBuiltRevision" => {"SHA1" => "481002a"}}], "url" => "/job/dr_feature/12/api/json"}
+      Status::Request.stub(:new => stub(:get => {"builds" => [build]}))
+      Status::Jenkins.new("dr/feature", '481002a').target_url.should == "/job/dr_feature/12/api/json"
+    end
+
+    it "has he last build number when the build is in the change set" do
+      build = {"number" => 12, "changeSet" => {"items" => [{"commitId" => "481002a"}]}, "actions" => [], "url" => "/job/dr_feature/12/api/json"}
+      Status::Request.stub(:new => stub(:get => {"builds" => [build]}))
+      Status::Jenkins.new("dr/feature", '481002a').target_url.should == "/job/dr_feature/12/api/json"
     end
   end
 end
